beaa14828666b5dba654e3d5862049883ba6cb6e
s2716746 <s2716746@ed.ac.uk>
2026-07-30 19:41:45 +0300

    download the models from huggingface

--- backend/app/routers/predict.py
+++ backend/app/routers/predict.py
@@ -1,16 +1,30 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from PIL import Image
-
 from app.services.ensemble_predictor import EnsemblePredictor
+from huggingface_hub import hf_hub_download
 
 router = APIRouter(
     prefix="/predict",
     tags=["Predict"],
 )
 
+CACHE_DIR = "models"
+
+efficientnet_checkpoint = hf_hub_download(
+    repo_id="FatimahAljishi/gleasonai-models",
+    filename="unet_epoch8.pt",
+    local_dir=CACHE_DIR,
+)
+
+resnext_checkpoint = hf_hub_download(
+    repo_id="FatimahAljishi/gleasonai-models",
+    filename="unet_epoch11.pt",
+    local_dir=CACHE_DIR,
+)
+
 predictor = EnsemblePredictor(
-    efficientnet_checkpoint="model/unet_epoch8.pt",
-    resnext_checkpoint="model/unet_epoch11.pt",
+    efficientnet_checkpoint=efficientnet_checkpoint,
+    resnext_checkpoint=resnext_checkpoint,
 )
 
 
